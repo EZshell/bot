a218042e60cb430f765f6e120466a7822fee20e2
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-13 22:10:16 +0330

    add on start

--- bot/index.ts
+++ bot/index.ts
@@ -1,23 +1,32 @@
 import { Bot } from "grammy";
+import { UserFromGetMe } from "grammy/out/types";
+import { BotToken, SuperAdmin } from "./config";
 import sequelize from "./database";
 import User from "./database/models/user.model";
 
-const bot = new Bot("5663656105:AAGutWXLmJmKETpCNfh6K92XKvFpMnyELOY"); // <-- put your authentication token between the ""
+const bot = new Bot(BotToken);
 
 
 // Handle the /start command.
 bot.command("start", async (ctx) => {
-    ctx.reply("Hello Rasoul! Up and running.")
-    try {
-        await sequelize.authenticate();
-        ctx.reply('Connection has been established successfully.');
-    } catch (error) {
-        ctx.reply('Unable to connect to the database:' + error);
-    }
+    ctx.reply("Hello Jigar! Up and running.")
+
 });
 
 // Handle other messages.
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
 
 
-bot.start();
+
+bot.start({
+    onStart: async (info: UserFromGetMe) => {
+        let _text = `<b>${info.first_name} ${info.last_name}</b> is running ...\n`
+        try {
+            await sequelize.authenticate();
+            _text = `<b>Database:</b> is connected & running well`
+        } catch (error) {
+            _text = `<b>Database:</b> Unable to connect to the database (${error})`
+        }
+        bot.api.sendMessage(SuperAdmin, _text, { parse_mode: 'HTML' })
+    }
+});
