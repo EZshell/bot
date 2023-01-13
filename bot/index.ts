49915bf6a01bbbe00b741ad5fd416d49c66bd80c
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-13 21:46:51 +0330

    add database

--- bot/index.ts
+++ bot/index.ts
@@ -1,7 +1,7 @@
 // import { Bot } from "grammy";
 
-import sequelize from "./database";
-import User from "./database/models/user.model";
+// import sequelize from "./database";
+// import User from "./database/models/user.model";
 
 
 import { Bot } from "https://deno.land/x/grammy@v1.13.0/mod.ts";
@@ -13,12 +13,12 @@ const bot = new Bot("5663656105:AAGutWXLmJmKETpCNfh6K92XKvFpMnyELOY"); // <-- pu
 // Handle the /start command.
 bot.command("start", async (ctx) => {
     ctx.reply("Hello Rasoul! Up and running.")
-    try {
-        await sequelize.authenticate();
-        ctx.reply('Connection has been established successfully.');
-    } catch (error) {
-        ctx.reply('Unable to connect to the database:' + error);
-    }
+    // try {
+    //     await sequelize.authenticate();
+    //     ctx.reply('Connection has been established successfully.');
+    // } catch (error) {
+    //     ctx.reply('Unable to connect to the database:' + error);
+    // }
 });
 // Handle other messages.
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
