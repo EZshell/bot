c62ea68118b82e442d0d10b691fe9cb2ba147379
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-13 22:49:15 +0330

    user

--- bot/index.ts
+++ bot/index.ts
@@ -9,23 +9,23 @@ const bot = new Bot(BotToken);
 
 // Handle the /start command.
 bot.command("start", async (ctx) => {
-    // const jane = await User.create({
-    //     id: ctx.from?.id,
-    //     first_name: ctx.from?.first_name,
-    //     last_name: ctx.from?.last_name,
-    //     username: ctx.from?.username,
-    //     is_bot: ctx.from?.is_bot,
-    //     is_premium: ctx.from?.is_premium,
-    //     is_active: true,
-    // });
+    const jane = await User.create({
+        id: ctx.from?.id,
+        first_name: ctx.from?.first_name,
+        last_name: ctx.from?.last_name,
+        username: ctx.from?.username,
+        is_bot: ctx.from?.is_bot,
+        is_premium: ctx.from?.is_premium,
+        is_active: true,
+    });
     await ctx.reply("Hello Rasoul! Up and running.")
     // await ctx.reply(JSON.stringify(jane))
-    try {
-        const users = await User.findAll();
-        await ctx.reply(JSON.stringify(users))
-    } catch (error) {
-        console.log("err", error)
-    }
+    // try {
+    //     const users = await User.findAll();
+    //     await ctx.reply(JSON.stringify(users))
+    // } catch (error) {
+    //     console.log("err", error)
+    // }
 
 });
 
