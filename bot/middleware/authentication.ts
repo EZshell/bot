528308aa72583c06e4981219ab6074c37f5486bd
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-14 12:21:54 +0330

    add server fixed

--- bot/middleware/authentication.ts
+++ bot/middleware/authentication.ts
@@ -4,33 +4,34 @@ import User from "../database/models/user.model";
 
 async function Authentication(ctx: MyContext, next: NextFunction) {
     console.log("@@@@@@@@@@")
-    const { user } = await ctx.getAuthor()
-
+    const user = ctx.from
     console.log(user)
-    let _user = await User.findByPk(user.id)
-    if (_user) {
-        await _user.update({
-            first_name: user.first_name,
-            last_name: user.last_name,
-            username: user.username,
-        })
-        ctx.session.isNew = false
-    } else {
-        _user = await User.create({
-            id: user.id,
-            first_name: user.first_name,
-            last_name: user.last_name,
-            username: user.username,
-            is_bot: user.is_bot,
-            is_premium: user.is_premium || false,
-            is_active: true,
-            servers: []
-        })
-        ctx.session.isNew = true
-    }
-
-    ctx.session.user = _user;
     await next();
+    return
+    // let _user = await User.findByPk(user.id)
+    // if (_user) {
+    //     await _user.update({
+    //         first_name: user.first_name,
+    //         last_name: user.last_name,
+    //         username: user.username,
+    //     })
+    //     ctx.session.isNew = false
+    // } else {
+    //     _user = await User.create({
+    //         id: user.id,
+    //         first_name: user.first_name,
+    //         last_name: user.last_name,
+    //         username: user.username,
+    //         is_bot: user.is_bot,
+    //         is_premium: user.is_premium || false,
+    //         is_active: true,
+    //         servers: []
+    //     })
+    //     ctx.session.isNew = true
+    // }
+
+    // ctx.session.user = _user;
+    // await next();
 }
 
 
