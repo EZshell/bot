40f3cc3a3b0de6692b2d18fee130c7c02a8537ed
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-15 23:08:46 +0330

    add snippets

--- bot/middleware/authentication.ts
+++ bot/middleware/authentication.ts
@@ -21,7 +21,8 @@ async function Authentication(ctx: MyContext, next: NextFunction) {
             is_bot: user.is_bot,
             is_premium: user.is_premium || false,
             is_active: true,
-            servers: "[]"
+            servers: "[]",
+            snippets: "[]"
         })
         if (ctx.session) ctx.session.isNew = true
     }
