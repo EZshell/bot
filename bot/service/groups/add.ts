02d9fe6ae4fd52e675b07f29694a6d7a8a3cc71c
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-01-17 03:28:24 +0330

    group list

--- bot/service/groups/add.ts
+++ bot/service/groups/add.ts
@@ -65,7 +65,7 @@ class AddGroupService {
             groups.push(d.id)
             await ctx.reply(JSON.stringify(groups))
             const u = await ctx.session.user!.update({ groups: JSON.parse(JSON.stringify(groups)) })
-            await ctx.reply(";ppp;")
+            await ctx.reply(";p55p;")
             await ctx.reply(JSON.stringify(u))
             await ctx.reply("✅ Group added:\n/groups", { reply_to_message_id: ctx.message?.message_id })
         }
